9e135d1007b277b7c928f0b040773bb4555f3cee
Subhadra S. Chakraborty <subhadrasundar@gmail.com>
2017-09-08 06:31:19 +0530

    First Commit

---
+++ src/linearregression/LinearRegression.java
@@ -0,0 +1,9 @@
+package linearregression;
+
+public class LinearRegression {
+    public static void main(String[] args) {
+        MainFrame f = new MainFrame();
+        f.setLocation(100, 50);
+        f.setVisible(true);
+    }
+}
